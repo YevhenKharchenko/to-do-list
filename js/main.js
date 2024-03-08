4b8a3118b9d2f4641d2e3d9b597c5f5e8e5f56e4
YevhenKharchenko <evgenvalerych@gmail.com>
2024-03-08 09:08:37 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('.form');
+const formInput = document.querySelector('.form-input');
+const list = document.querySelector('.list');
+const formBtn = document.querySelector('.form-btn');
+
+form.addEventListener('submit', onFormBtnClick);
+
+function onFormBtnClick(event) {
+  event.preventDefault();
+
+  if (formInput.value) {
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-item');
+    const itemDeleteBtn = document.createElement('button');
+    itemDeleteBtn.classList.add('item-delete-btn');
+    listItem.textContent = formInput.value;
+    const deleteBtnIcon = document.createElement('img');
+    deleteBtnIcon.setAttribute('src', './images/Trash.svg');
+    deleteBtnIcon.classList.add('delete-btn-icon');
+    itemDeleteBtn.append(deleteBtnIcon);
+    listItem.append(itemDeleteBtn);
+    list.append(listItem);
+    form.reset();
+
+    itemDeleteBtn.addEventListener('click', onItemDeleteBtn);
+
+    function onItemDeleteBtn() {
+      listItem.remove();
+    }
+  }
+}
